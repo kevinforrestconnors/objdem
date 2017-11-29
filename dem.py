afee7b8ecb42e4aa7b38d56d505e8129afafa659
Kevin Connors <kevinforrestconnors@gmail.com>
2017-11-29 11:37:25 -0500

    map of world

--- dem.py
+++ dem.py
@@ -5,20 +5,22 @@ from owslib.wms import WebMapService
 
 wms = WebMapService('http://webmap.ornl.gov/ogcbroker/wms', version='1.1.1')
 
-layer = list(wms.contents)[1]
+gtopo = '10003_1'
 
-print(wms[layer].title)
-print(wms[layer].queryable)
-print(wms[layer].opaque)
-print(wms[layer].boundingBox)
-print(wms[layer].boundingBoxWGS84)
-print(wms[layer].crsOptions)
-print(wms[layer].styles)
 
-get_map = wms.getOperationByName('GetMap')
-print(get_map.formatOptions)
 
-img = wms.getmap(layers=[layer],
+print(wms[gtopo].title)
+print(wms[gtopo].queryable)
+print(wms[gtopo].opaque)
+print(wms[gtopo].boundingBox)
+print(wms[gtopo].boundingBoxWGS84)
+print(wms[gtopo].crsOptions)
+print(wms[gtopo].styles)
+
+get_feature_info = wms.getOperationByName('GetFeatureInfo')
+print(get_feature_info.formatOptions)
+
+img = wms.getmap(layers=[gtopo],
                  styles=['default'],
                  srs='EPSG:4326',
                  bbox=(-180, -90, 180, 90),
--- tests.py
+++ tests.py
@@ -16,9 +16,16 @@ def web_map_service():
     assert_equal(wms.identification.type, 'OGC:WMS', "WMS Type Is Correct")
     assert_equal(wms.identification.title, 'ORNL DAAC WMS Server', "WMS Title is Correct")
 
+def gtopo_is_10003_1():
+    wms = WebMapService('http://webmap.ornl.gov/ogcbroker/wms', version='1.1.1')
+    for name in list(wms.contents):
+        if name == '10003_1':
+            print(wms[name].title)
+            assert_equal(wms[name].title, 'GLOBAL 30 ARC-SECOND ELEVATION (GTOPO30)', "gtopo_is_10003_1")
 
 def main():
     web_map_service()
+    gtopo_is_10003_1()
     print(str(len(testsPassed)) + " tests passed.")
     print(str(len(testsFailed)) + " tests failed.")
     print("Tests failed: ")
