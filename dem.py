7114eed5e2a6c44f9741d0ce97f82bd6bd77dbfd
Kevin Connors <connorsk@grinnell.edu>
2017-11-30 11:08:14 -0600

    decided to use openurl rather than owslib

--- dem.py
+++ dem.py
@@ -1,33 +1,57 @@
-from owslib.wms import WebMapService
+import urllib.request
+import numpy
+import array
+import sys
+
+elevation_data = []
 
 
 
+def fetch_elevation_data(min_long=-113.36, min_lat=36.0, max_long=-113.13, max_lat=36.23, resolution=30):
 
-wms = WebMapService('http://webmap.ornl.gov/ogcbroker/wms', version='1.1.1')
+    if (resolution < 30):
+        resolution = 30
 
-gtopo = '10003_1'
+    m_per_deg_lat = 111619
+    deg_per_m = resolution / m_per_deg_lat
 
+    long_range = max_long - min_long
+    lat_range = max_lat - min_lat
 
+    width = round(long_range / deg_per_m)
+    height = round(lat_range / deg_per_m)
 
-print(wms[gtopo].title)
-print(wms[gtopo].queryable)
-print(wms[gtopo].opaque)
-print(wms[gtopo].boundingBox)
-print(wms[gtopo].boundingBoxWGS84)
-print(wms[gtopo].crsOptions)
-print(wms[gtopo].styles)
+    res = urllib.request.urlopen('https://data.worldwind.arc.nasa.gov/elev?'
+                                 'service=WMS'
+                                 '&request=GetMap'
+                                 '&layers=NED'
+                                 '&crs=EPSG:4326'
+                                 '&format=image/bil'
+                                 '&transparent=FALSE'
+                                 '&width=' + str(width) +
+                                 '&height=' + str(height) +
+                                 '&bgcolor=0xFFFFFF'
+                                 '&bbox=' + str(min_long) + ',' + str(min_lat) + ',' + str(max_long) + ',' + str(max_lat) +
+                                 '&styles='
+                                 '&version=1.3.0')
 
-get_feature_info = wms.getOperationByName('GetFeatureInfo')
-print(get_feature_info.formatOptions)
+    f = open('data.bil', 'wb')
+    bytes_written = f.write(res.read())
+    f.close()
 
-img = wms.getmap(layers=[gtopo],
-                 styles=['default'],
-                 srs='EPSG:4326',
-                 bbox=(-180, -90, 180, 90),
-                 size=(600, 300),
-                 format='image/tiff',
-                 transparent=True
-                 )
-out = open('jpl_mosaic_visb.jpg', 'wb')
-out.write(img.read())
-out.close()
+    # Read from file again
+    b = array.array("h")
+    with open("data.bil", "rb") as f:
+        b.fromfile(f, width * height)
+    if sys.byteorder == "big":
+        b.byteswap()
+
+    for y in range(0, height):
+        row = []
+        for x in range(0, width):
+            start = height * y
+            row.append(b[start + x])
+        elevation_data.append(row)
+
+fetch_elevation_data()
+print(elevation_data)
