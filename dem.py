16c11b0f0dd35e5b6e6d96137b8c5b08c7f5300e
Kevin Connors <kevinforrestconnors@gmail.com>
2017-12-01 17:21:40 -0500

    fix delauny triangles at edges

--- dem.py
+++ dem.py
@@ -2,11 +2,12 @@ import sys
 import os
 import array
 import urllib.request
-from scipy.spatial import Delaunay
+import math
 import numpy as np
+from scipy.spatial import Delaunay
+import utm
 
 elevation_data = []
-
 m_per_deg_lat = 111619
 
 def fetch_elevation_data(min_long=-113.36, min_lat=36.0, max_long=-113.13, max_lat=36.23, resolution=30):
@@ -52,11 +53,11 @@ def fetch_elevation_data(min_long=-113.36, min_lat=36.0, max_long=-113.13, max_l
         for y in range(0, height):
             start = height * x
             row.append(b[start + y])
-        print(len(row))
         elevation_data.append(row)
 # end function
 
-def elevation_points_to_long_lat(min_long=-113.36, min_lat=36.0, max_long=-113.13, max_lat=36.23, resolution=30):
+
+def elevation_points_to_xyz(min_long=-113.36, min_lat=36.0, max_long=-113.13, max_lat=36.23, resolution=30):
 
     resolution_in_deg = resolution / m_per_deg_lat
 
@@ -64,28 +65,56 @@ def elevation_points_to_long_lat(min_long=-113.36, min_lat=36.0, max_long=-113.1
 
     for i in range(0, len(elevation_data)):
         for j in range(0, len(elevation_data[0])):
-            x = j#min_long + resolution_in_deg * j
-            y = i#min_lat + resolution_in_deg * i
+            long = min_long + resolution_in_deg * j
+            lat = min_lat + resolution_in_deg * i
+            (x, y, _, _) = utm.from_latlon(lat, long)
             z = elevation_data[i][j]
             element = [x, y, z]
             data.append(element)
 
-    return data
+    xs = list(map(lambda e: e[0], data))
+    ys = list(map(lambda e: e[1], data))
+    zs = list(map(lambda e: e[2], data))
+
+    mean_x = math.floor(max(xs) / min(xs))
+    mean_y = math.floor(max(ys) / min(ys))
+    min_z = math.floor(min(zs))
+
+    # min_x = min(xs)
+    # max_x = max(xs)
+    # min_y = min(ys)
+    # max_y = max(ys)
+
+    data.append([min(xs) - 10, min(ys) - 10, min_z - 1])
+    data.append([min(xs) - 10, max(ys) + 10, min_z - 1])
+    data.append([max(xs) + 10, min(ys) - 10, min_z - 1])
+    data.append([max(xs) + 10, max(ys) + 10, min_z - 1])
+
+    return list(map(lambda e: [e[0] - mean_x, e[1] - mean_y, e[2] - min_z], data))
 # end function
+
 
 def write_points_to_obj():
 
     os.remove("model.obj")
     f = open("model.obj", 'a')
 
-    mlong=-80
-    mlat=37.5
+    minlong=-80
+    minlat=37.5
     maxlong=-79.5
     maxlat=38
     resolution=90
 
-    fetch_elevation_data(min_long=mlong, min_lat=mlat, max_long=maxlong, max_lat=maxlat, resolution=resolution)
-    long_lat_data = elevation_points_to_long_lat(min_long=mlong, min_lat=mlat, max_long=maxlong, max_lat=maxlat, resolution=resolution)
+    resolution_in_deg = resolution / m_per_deg_lat
+
+    long_range = maxlong - minlong
+    lat_range = maxlat - minlat
+
+    width = round(long_range / resolution_in_deg)
+    height = round(lat_range / resolution_in_deg)
+
+    fetch_elevation_data(min_long=minlong, min_lat=minlat, max_long=maxlong, max_lat=maxlat, resolution=resolution)
+    long_lat_data = elevation_points_to_xyz(min_long=minlong, min_lat=minlat, max_long=maxlong, max_lat=maxlat, resolution=resolution)
 
     for point in long_lat_data:
         f.write("v " + str(point[0]) + " " + str(point[1]) + " " + str(point[2]) + '\n')
@@ -94,8 +123,30 @@ def write_points_to_obj():
 
     delauny = Delaunay(long_lat_minus_elevation)
 
+    limit = width*height
+
+    a = len(long_lat_data) - 1
+    b = len(long_lat_data) - 2
+    c = len(long_lat_data) - 3
+    d = len(long_lat_data) - 4
+
     for simplex in delauny.simplices:
-        f.write("f " + str(simplex[0] + 1) + " " + str(simplex[1] + 1) + " " + str(simplex[2] + 1) + '\n')
+        # don't compute for (0,0) or (width,height)
+        if (simplex[0] != a and
+            simplex[1] != a and
+            simplex[2] != a and
+            simplex[0] != b and
+            simplex[1] != b and
+            simplex[2] != b and
+            simplex[0] != c and
+            simplex[1] != c and
+            simplex[2] != c and
+            simplex[0] != d and
+            simplex[1] != d and
+            simplex[2] != d):
+            f.write("f " + str(simplex[0] + 1) + " " + str(simplex[1] + 1) + " " + str(simplex[2] + 1) + '\n')
 
     f.close()
 # end function
+
+write_points_to_obj()
