a2315c7d07d5dd1981c20d473da386b2663399db
Kevin Connors <connorsk@grinnell.edu>
2017-12-04 18:00:04 -0500

    fixes email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Generates an .obj file representing a digital elevation map from coordinate input',
     license="MIT",
     author='Kevin Forrest Connors',
-    author_email='kevinforrestconnors.com@gmail.com',
+    author_email='kevinforrestconnors@gmail.com',
     url="https://github.com/kevinforrestconnors/objdem",
     packages=['objDEM'],
     install_requires=['numpy', 'scipy', 'utm'],
