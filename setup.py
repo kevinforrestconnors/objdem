076e9e90118ebcf3b53170d322f528896c6b3eb6
Kevin Connors <connorsk@grinnell.edu>
2017-12-05 10:24:59 -0500

    updates version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='objDEM',
-    version='1.0.9',
+    version='1.0.10',
     description='Generates an .obj file representing a digital elevation map from coordinate input',
     license="MIT",
     author='Kevin Forrest Connors',
