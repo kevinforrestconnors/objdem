57ba56892f12f0a7f3fb031c98ce4fddf1a604f9
Kevin Connors <connorsk@grinnell.edu>
2017-12-08 15:21:46 -0500

    fix bug where non-square data was malformed

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name='objDEM',
-    version='1.0.10',
+    version='1.0.12',
     description='Generates an .obj file representing a digital elevation map from coordinate input',
     license="MIT",
     author='Kevin Forrest Connors',
     author_email='kevinforrestconnors@gmail.com',
     url="https://github.com/kevinforrestconnors/objDEM",
     packages=['objDEM'],
-    install_requires=['numpy', 'scipy', 'utm'],
-    scripts=['bin/objDEM']
+    install_requires=['numpy', 'scipy', 'utm']
 )
